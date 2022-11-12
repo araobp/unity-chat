5badb82454db801856821aff0752a2d500c0e65c
araobp <araobp@users.noreply.github.com>
2022-11-12 23:10:07 +0900

    FZH house and First Person Controller

---
+++ SalesforceChat/Assets/Scripts/Visitor.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
+
+public class Visitor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var keyboard = Keyboard.current;
+        if (keyboard.wKey.isPressed)
+        {
+
+        } else if (keyboard.aKey.isPressed)
+        {
+
+        } else if (keyboard.sKey.isPressed)
+        {
+
+        } else if (keyboard.dKey.isPressed)
+        {
+
+        }
+           
+    }
+}
